404f0657e2952e47b787ce5407620411c638736e
hellen truong <hellentruong123@gmail.com>
2026-04-01 20:54:48 +0200

    create seller signup form

--- src/app/views/seller/seller.component.ts
+++ src/app/views/seller/seller.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller.component.html',
-  styleUrl: './seller.component.css'
+  styleUrl: './seller.component.css',
 })
 export class SellerComponent {
-
+  signUp(data: object): void {
+    console.warn(data);
+  }
 }
