0e967d637e949a513c9f6d68a74e97d90bc4acd8
hellen truong <hellentruong123@gmail.com>
2026-04-01 15:15:31 +0200

    create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { SellerComponent } from './views/seller/seller.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'seller', component: SellerComponent },
+];
