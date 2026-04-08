bca3d39bb2901d53dd6603530c9f3b580b1d2226
hellen truong <hellentruong123@gmail.com>
2026-04-08 10:09:23 +0200

    control route access for seller pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './views/home/home.component';
 import { SellerComponent } from './views/seller/seller.component';
 import { SellerAccountComponent } from './views/seller-account/seller-account.component';
 import { authGuard } from './auth.guard';
+import { SellerAddProductComponent } from './views/seller-add-product/seller-add-product.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,6 +11,11 @@ export const routes: Routes = [
   {
     path: 'seller-account',
     component: SellerAccountComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'seller-add-product',
+    component: SellerAddProductComponent,
     canActivate: [authGuard],
   },
 ];
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,10 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { SellerService } from './services/seller.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const sellerService = inject(SellerService);
 
-  const seller = localStorage.getItem('seller');
+  const seller = sellerService.user;
 
   if (seller) {
     return true;
