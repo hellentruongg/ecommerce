c006494a7231ed137e839fc90e4fd24a7e1dfdaf
hellen truong <hellentruong123@gmail.com>
2026-04-09 15:07:43 +0200

    create seller add-product form

--- src/app/views/seller-add-product/seller-add-product.component.ts
+++ src/app/views/seller-add-product/seller-add-product.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-add-product',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './seller-add-product.component.html',
-  styleUrl: './seller-add-product.component.css'
+  styleUrl: './seller-add-product.component.css',
 })
 export class SellerAddProductComponent {
-
+  addProduct(data: Object): void {
+    console.warn(data);
+  }
 }
